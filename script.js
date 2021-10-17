867221c39d6e79f06dfab499598ea9c1b251d533
Amoorer75 <Amoorer127@gmail.com>
2021-10-17 18:44:16 -0400

    answers

--- script.js
+++ script.js
@@ -20,16 +20,41 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
+  document.querySelector('#main-title').textContent = "DOM Toretto homepage";
 
   // Part 2
-
+  document.querySelector('body').style.background = "green"
   // Part 3
-
-  // Part 4
-
-  // Part 5
-
+  document.querySelector("li:last-child").remove('#favorite-things');
+       
+  // Part 5"
+  let ulElem = document.getElementById("past-races")
+  // I do not know why but to remove this i had to put 7 insted of 3
+  ulElem.removeChild(ulElem.childNodes[7])
   // Part 6
-
+   let node = document.createElement('li')
+   let text = document.createTextNode("New York")
+   node.appendChild(text);
+   document.querySelector("#past-races").appendChild(node)
   // Part 7
+ 
+  let domA = document.querySelector('.main')
+
+  let node1 = document.createElement('Div')
+  node1.className = 'blog-post purple'
+
+  let h1 = document.createElement('H1')
+  h1.innerText = 'New York'
+
+  let p1 = document.createElement('P')
+  p1.innerText = 'I blew somthing up'
+
+  domA.appendChild(node1);
+  node1.appendChild(h1);
+  node1.appendChild(p1)
+// Part 4
+document.querySelectorAll(".special-title").style.fontSize = "2 rem"
+  
+
 }
+document
